caf6eeb6b7fa522ace6500342bff1606d17f0c88
kirtigalani <kittugalani@gmail.com>
2024-12-04 17:57:17 +0530

    Add project files.

---
+++ Method/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program.even(30);
+            Program p = new Program();
+            int sum = p.add(20, 20);
+            Console.WriteLine("sum: " + sum);
+        }
+
+        public int add(int fn,int sn)
+        {
+            return fn + sn;
+        }
+        public static void even(int target)
+        {
+            int start = 0;
+             
+            while(start<=target)
+            {
+                Console.WriteLine(start);
+                start = start + 2;
+            }
+        }
+    }
+}
